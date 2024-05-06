6f8d6f46655713e854463cb9d9e62be3d918a2ee
C-Sweet-Person <106597560+C-Sweet-Person@users.noreply.github.com>
2024-05-06 15:00:36 -0300

    Getting ready for new features (classes for NPC)

---
+++ npc.js
@@ -0,0 +1,10 @@
+class npc {
+    constructor(x,y,width,height, color)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+}
